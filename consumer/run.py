5fcfb3d8663a36ac0cf9ccb66d1ecb0c7827f31a
Ash2023qqq <129294974+Ash2023qqq@users.noreply.github.com>
2024-03-11 01:07:19 +0530

    Update run.py [skip ci]

--- consumer/run.py
+++ consumer/run.py
@@ -1,12 +1,29 @@
-import os
+# import os
+# import json
+
+# postreqdata = json.loads(open(os.environ['req']).read())
+# print("Received events: {}".format(postreqdata))
+
+# response = open(os.environ['res'], 'w')
+# for event in postreqdata:
+#     event_data = event['data']
+#     print("Got a custom event {}".format(event_data))
+
+# response.close()
+
 import json
+import logging
 
-postreqdata = json.loads(open(os.environ['req']).read())
-print("Received events: {}".format(postreqdata))
+import azure.functions as func
 
-response = open(os.environ['res'], 'w')
-for event in postreqdata:
-    event_data = event['data']
-    print("Got a custom event {}".format(event_data))
+def main(event: func.EventGridEvent):
 
-response.close()
+    result = json.dumps({
+        'id': event.id,
+        'data': event.get_json(),
+        'topic': event.topic,
+        'subject': event.subject,
+        'event_type': event.event_type,
+    })
+
+    logging.info('Python EventGrid trigger processed an event: %s', result)
