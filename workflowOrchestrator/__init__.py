ab3fdf8cd7aaf93a28f96441f221ca6985e3592a
Ash2023qqq <129294974+Ash2023qqq@users.noreply.github.com>
2024-03-11 14:35:30 +0530

    Create __init__.py

---
+++ workflowOrchestrator/__init__.py
@@ -0,0 +1,20 @@
+import logging
+import json
+
+from azure.durable_functions import DurableOrchestrationContext, Orchestrator
+
+
+def orchestrator_function(context: DurableOrchestrationContext):
+    event_data = context.get_input()
+    logging.info(f"workflow_orchestrator invoked, received event_data: {event_data}")
+    print(f"workflow_orchestrator invoked, received event_data: {event_data}")
+    model_requested = event_data["model"]
+    if model_requested == "consumer":
+        result = yield context.call_activity("consumer", event_data)
+    #elif model_requested == "aiaas_sda_vitals":
+    #    result = yield context.call_activity("aiaas_sda_vitals", event_data)
+    else :
+        result = "Model not found"
+    return result
+
+main = Orchestrator.create(orchestrator_function)
