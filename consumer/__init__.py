00be771b6a16e29acf6597e738473d9dae7d5b81
Ash2023qqq <129294974+Ash2023qqq@users.noreply.github.com>
2024-03-11 14:59:32 +0530

    Update __init__.py [skip ci]

--- consumer/__init__.py
+++ consumer/__init__.py
@@ -20,7 +20,6 @@ def main(event: func.EventGridEvent):
 
     result = json.dumps({
         'id': event.id,
-        'model': event.model,
         'data': event.get_json(),
         'topic': event.topic,
         'subject': event.subject,
