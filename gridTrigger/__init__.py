120a3e7efdacc002c4c9f7bd670a05746b321125
Ash2023qqq <129294974+Ash2023qqq@users.noreply.github.com>
2024-03-12 01:47:02 +0530

    Update __init__.py

--- gridTrigger/__init__.py
+++ gridTrigger/__init__.py
@@ -8,8 +8,8 @@ import azure.durable_functions as df
 async def main(events: EventGridEvent, starter: str):
     client = df.DurableOrchestrationClient(starter)
     #for event in events:
-    logging.info('Python Eventgrid trigger processed an event: %s',
-                  event.get_body().decode('utf-8'))
+    event = events
+    logging.info('Python Eventgrid trigger processed an event: %s', event.get_body().decode('utf-8'))
     event_data = json.loads(event.get_body().decode('utf-8'))
     instance_id = await client.start_new('workflowOrchestrator', None, event_data)
     logging.info(f"Started orchestration with ID = '{instance_id}'.")
