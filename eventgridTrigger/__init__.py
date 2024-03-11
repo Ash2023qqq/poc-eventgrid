11a692812329331101ab5df2add0fcb1fd448bc5
Ash2023qqq <129294974+Ash2023qqq@users.noreply.github.com>
2024-03-11 16:21:09 +0530

    Create __init__.py

---
+++ eventgridTrigger/__init__.py
@@ -0,0 +1,16 @@
+import logging
+import json
+from azure.functions import EventGridEvent
+from typing import List
+import azure.durable_functions as df
+
+
+async def main(events: List[EventGridEvent], starter: str):
+    client = df.DurableOrchestrationClient(starter)
+    for event in events:
+        logging.info('Python Eventgrid trigger processed an event: %s',
+                      event.get_body().decode('utf-8'))
+        event_data = json.loads(event.get_body().decode('utf-8'))
+        instance_id = await client.start_new('workflowOrchestrator', None, event_data)
+        logging.info(f"Started orchestration with ID = '{instance_id}'.")
+        print(f"Started orchestration with ID = '{instance_id}'.")       
