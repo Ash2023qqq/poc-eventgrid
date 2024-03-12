96cd6e1ca8b4b2419c93d0d6bb63a57d72800e09
Ash2023qqq <129294974+Ash2023qqq@users.noreply.github.com>
2024-03-13 00:11:41 +0530

    Update __init__.py [skip ci]

--- consumer/__init__.py
+++ consumer/__init__.py
@@ -1,17 +1,24 @@
 import json
 import logging
-
+import datetime
 import azure.functions as func
 
-def main(event: str):
-
-    result = json.dumps({
-        'id': "event.id",
-        'data': event,
-        'topic': "event.topic",
-        'subject': "event.subject",
-        'eventtype': "event.eventtype",
-    })
-
-    logging.info('Python EventGrid trigger processed an event: %s', result)
-    return result
+def main(event: str, outputEvent: func.Out[func.EventGridOutputEvent]):
+    logging.log("eventGridEvent: ", event)
+    # result = json.dumps({
+    #     'id': "event.id",
+    #     'data': event,
+    #     'topic': "event.topic",
+    #     'subject': "event.subject",
+    #     'eventtype': "event.eventtype",
+    # })
+    outputEvent.set(
+        func.EventGridOutputEvent(
+            id="test-id",
+            data={"tag1": "value1", "tag2": "value2"},
+            subject="test-subject",
+            event_type="test-event-1",
+            event_time=datetime.datetime.utcnow(),
+            data_version="1.0"))
+    logging.info('Python EventGrid trigger processed an event: %s', event)
+    return 0
