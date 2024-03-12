bd01a90bbbd0e7a2e7a258756bbd4d5fcd5c4632
Ash2023qqq <129294974+Ash2023qqq@users.noreply.github.com>
2024-03-12 12:21:27 +0530

    Update __init__.py

--- consumer/__init__.py
+++ consumer/__init__.py
@@ -10,7 +10,7 @@ def main(event: str):
         'data': event,
         'topic': "event.topic",
         'subject': "event.subject",
-        'event_type': "event.event_type",
+        'event_type': "event.eventtype",
     })
 
     logging.info('Python EventGrid trigger processed an event: %s', result)
