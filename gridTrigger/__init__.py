adf43af07895477c67baa832b85f905f93bcfeea
Ash2023qqq <129294974+Ash2023qqq@users.noreply.github.com>
2024-03-18 00:04:03 +0530

    Update __init__.py

--- gridTrigger/__init__.py
+++ gridTrigger/__init__.py
@@ -7,12 +7,12 @@ import azure.durable_functions as df
 
 async def main(events: EventGridEvent, starter: str):
     client = df.DurableOrchestrationClient(starter)
-    for event in events:
-        #event = events
-        #logging.info('Python Eventgrid trigger processed an event: %s', event.get_json())
-        logging.info('Python Eventgrid trigger processed an event: %s', event.get_body().decode('utf-8'))
-        #event_data = event.get_json()
-        event_data = json.loads(event.get_body().decode('utf-8'))
-        instance_id = await client.start_new('workflowOrchestrator', None, event_data)
-        logging.info(f"Started orchestration with ID = '{instance_id}'.")
-        print(f"Started orchestration with ID = '{instance_id}'.")       
+    #for event in events:
+    event = events
+    logging.info('Python Eventgrid trigger processed an event: %s', event.get_json())
+    #logging.info('Python Eventgrid trigger processed an event: %s', event.get_body().decode('utf-8'))
+    event_data = event.get_json()
+    #    event_data = json.loads(event.get_body().decode('utf-8'))
+    instance_id = await client.start_new('workflowOrchestrator', None, event_data)
+    logging.info(f"Started orchestration with ID = '{instance_id}'.")
+    print(f"Started orchestration with ID = '{instance_id}'.")       
