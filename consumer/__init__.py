3419bf18c2343dd0cfac1e772237de63644f4c74
Ash2023qqq <129294974+Ash2023qqq@users.noreply.github.com>
2024-03-17 23:45:23 +0530

    Update __init__.py

--- consumer/__init__.py
+++ consumer/__init__.py
@@ -8,18 +8,19 @@ def main(event: str, outputEvent: func.Out[func.EventGridOutputEvent]):
     logging.info('Python EventGrid trigger received an incoming event: %s', event)
     # result = json.dumps({
     #     'id': "event.id",
-    #     'data': event,
+    #     'data': event,data={"tag1": "value1", "tag2": "value2"},
     #     'topic': "event.topic",
     #     'subject': "event.subject",
     #     'eventtype': "event.eventtype",
     # })
+    result = json.loads(event)
     outputEvent.set(
         func.EventGridOutputEvent(
-            id="test-id",
-            data={"tag1": "value1", "tag2": "value2"},
-            subject="test-subject",
-            event_type="test-event-1",
+            id="result.id",
+            data=result,
+            subject="result.subject",
+            event_type="result.eventtype",
             event_time=datetime.datetime.utcnow(),
             data_version="1.0"))
-    logging.info('Python EventGrid trigger processed an event: %s', event)
+    logging.info('Python EventGrid trigger processed an event: %s', result)
     return 0
