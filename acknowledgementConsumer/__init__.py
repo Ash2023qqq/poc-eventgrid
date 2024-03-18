83c8b45bcb6b286c9f1f3f4b0cd73e4462ab8edd
Ash2023qqq <129294974+Ash2023qqq@users.noreply.github.com>
2024-03-18 13:05:05 +0530

    Update __init__.py

--- acknowledgementConsumer/__init__.py
+++ acknowledgementConsumer/__init__.py
@@ -6,10 +6,10 @@ import azure.functions as func
 def main(event: func.EventGridEvent):
     result = json.dumps({
         'id': "event.id",
-        'data': event.get_json(),
+        'data': "event.data",
         'topic': "event.topic",
         'subject': "event.subject",
-        'eventtype': "event.eventtype",
+        'eventtype': "event.event_type",
     })
     print(result)
     logging.info('Python EventGrid trigger : The acknowledgment from the ack topic is processed: %s', event)
