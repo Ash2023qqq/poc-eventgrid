a968ba4e4af02030adf0d99f67d54c94510c694d
Ash2023qqq <129294974+Ash2023qqq@users.noreply.github.com>
2024-03-18 13:57:38 +0530

    Update __init__.py

--- acknowledgementConsumer/__init__.py
+++ acknowledgementConsumer/__init__.py
@@ -4,13 +4,17 @@ import datetime
 import azure.functions as func
 
 def main(event: func.EventGridEvent):
-    result = json.dumps({
-        'id': "event.id",
-        'data': "event.data",
-        'topic': "event.topic",
-        'subject': "event.subject",
-        'eventtype': "event.event_type",
-    })
-    print(result)
-    logging.info('Python EventGrid trigger : The acknowledgment from the ack topic is processed: %s', event)
+    try:
+        result = json.dumps({
+            'id': "event.id",
+            'data': "event.data",
+            'topic': "event.topic",
+            'subject': "event.subject",
+            'eventtype': "event.event_type",
+        })
+        print(result)
+        logging.info('Python EventGrid trigger : %s', result)
+        logging.info('Python EventGrid trigger : The acknowledgment from the ack topic is processed: %s', event)
     #return result
+    except Exception as e:
+        logging.error(f'Unhandled error: {str(e)}')
